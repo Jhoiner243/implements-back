c167b6f69f673014af3786ee7969198dd23cc39a
Jhoiner243 <jhoinermartinez243@gmail.com>
2025-05-19 11:22:35 -0500

    fix: remove default SMTP configuration values for security reasons

--- src/config/smtp.config.ts
+++ src/config/smtp.config.ts
@@ -1,12 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const {
-  SMTP_HOST = "smtp.gmail.com",
-  SMTP_PORT = 465,
-  SECURE = true,
-  SMTP_AUTH_USER = "Jhoinermartinez243@gmail.com",
-  SMTP_AUTH_PASS = "avwl swrh taln ivtw",
-} = process.env;
+export const { SMTP_HOST, SMTP_PORT, SECURE, SMTP_AUTH_USER, SMTP_AUTH_PASS } =
+  process.env;
 
 console.log(SMTP_HOST, SMTP_PORT, SECURE, SMTP_AUTH_USER, SMTP_AUTH_PASS);
