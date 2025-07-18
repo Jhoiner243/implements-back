0d10c6280e6972a2576c2e7a41840fc325bf66c0
Jhoiner243 <jhoinermartinez243@gmail.com>
2025-07-18 11:06:13 -0500

    fix(server): validate port type in ServerSetup class
    - Added a check to ensure the port is a number before starting the server, improving error handling in the create method of the ServerSetup class.

--- src/frameworks/setups/server.setup.ts
+++ src/frameworks/setups/server.setup.ts
@@ -1,15 +1,17 @@
-import { Express } from 'express';
-import { injectable } from 'inversify';
-import { Port } from '../../ts/types/port';
-import { logger } from '../../utils/logger';
+import { Express } from "express";
+import { injectable } from "inversify";
+import { Port } from "../../ts/types/port";
+import { logger } from "../../utils/logger";
 
 @injectable()
-export class ServerSetup{
-   create(app: Express, port: Port){
-   const  server =  app.listen(port, () => {
-    logger.info(`Server is running at http://localhost:${port}`);
-
-   })
-   return server
+export class ServerSetup {
+  create(app: Express, port: Port) {
+    if (typeof port !== "number") {
+      throw new Error("Port must be a number");
+    }
+    const server = app.listen(port, "0.0.0.0", () => {
+      logger.info(`Server is running at ${port}`);
+    });
+    return server;
   }
 }
