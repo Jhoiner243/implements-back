d4983843525983de28d4bfb69236376269be3a8a
Jhoiner243 <jhoinermartinez243@gmail.com>
2025-07-18 22:30:50 -0500

    fix(server): simplify port handling in ServerSetup class
    - Removed the type check for the port in the create method, directly converting it to a number when starting the server, streamlining the server initialization process.

--- src/frameworks/setups/server.setup.ts
+++ src/frameworks/setups/server.setup.ts
@@ -6,10 +6,7 @@ import { logger } from "../../utils/logger";
 @injectable()
 export class ServerSetup {
   create(app: Express, port: Port) {
-    if (typeof port !== "number") {
-      throw new Error("Port must be a number");
-    }
-    const server = app.listen(port, "0.0.0.0", () => {
+    const server = app.listen(Number(port), "0.0.0.0", () => {
       logger.info(`Server is running at ${port}`);
     });
     return server;
